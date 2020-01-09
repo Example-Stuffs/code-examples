f3379868b3f192b2b456bc56b79d98a43a5273d9
Jackson Powell <jjpowell88@gmail.com>
2020-01-09 15:45:23 -0500

    Added HTTP to HTTPS with Spring example

---
+++ force_https_with_spring.java
@@ -0,0 +1,19 @@
+package com.redfirehose.wss.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class MvcConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    // Useful when your program is running on an untrusted network and there is no other mechanism handling HTTP to HTTPS redirects.
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.portMapper().http(80).mapsTo(443);
+        http.requiresChannel().anyRequest().requiresSecure();
+    }
+}
--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 Repo File | Description
 --------- | -----------
 [API Gateway with Node](https://github.com/JJPowell/js-examples/blob/master/api_gateway_example.js) | Use Node to handle a POST request containing JSON, validate it, forward it to some destination, then return the result to the user.
+[Force HTTP requests to HTTPS with Spring Boot](https://github.com/JJPowell/js-examples/blob/master/force_https_with_spring.java)
